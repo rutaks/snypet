428e3b121754693bbd95b79512fa2c8171de6e95
rutaks <samrutakayile@awesomity.rw>
2020-01-25 11:30:39 +0200

    Minor Changes

--- app/src/main/java/rw/rutaks/snypet/LoginActivity.java
+++ app/src/main/java/rw/rutaks/snypet/LoginActivity.java
@@ -1,14 +1,73 @@
 package rw.rutaks.snypet;
 
 import androidx.appcompat.app.AppCompatActivity;
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
+    //VIEW FIELDS DECLARATIONS
+    @BindView(R.id.editTextEmail) EditText editTextEmail;
+    @BindView(R.id.editTextPassword) EditText editTextPassword;
+    @BindView(R.id.buttonLogin)Button buttonLogin;
+    @BindView(R.id.buttonGoToRegister)Button buttonGoToRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
+
+        setupLoginButton();
+        setupGoToRegisterButton();
+    }
+
+    /**
+     * Override Method to redirect user to Main on authentication success
+     * i.e: if he/she already logged in
+     */
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
+        if(currentUser != null) goToMain();
+    }
+
+    /**
+     * Setup of register button to redirect to register view On Click
+     */
+    private void setupGoToRegisterButton() {
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+
+    /**
+     * Setup of login button to attempt login On Click
+     */
+    private void setupLoginButton() {
+        buttonGoToRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+
+    private void goToMain(){
+        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
--- app/src/main/java/rw/rutaks/snypet/MainActivity.java
+++ app/src/main/java/rw/rutaks/snypet/MainActivity.java
@@ -2,7 +2,11 @@ package rw.rutaks.snypet;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * Override Method to redirect user to login on authentication failure
+     */
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
+        if(currentUser == null) goToLogin();
+    }
+
+    /**
+     * Function to go to Login activity,
+     * Will Close Current MainActivity afterwards
+     */
+    public void goToLogin(){
+        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
